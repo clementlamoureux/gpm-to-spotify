ecd8c94e86b0253bf1b8708ca6375bdd694315e2
Clément LAMOUREUX <clement@raccourci.fr>
2015-04-17 18:17:13 +0200

    Can now specify playlist

--- src/run.js
+++ src/run.js
@@ -37,47 +37,62 @@ pm.init(config, function() {
         _.each(allDatas, function(pl, key){
             console.log('Playlist ' + chalk.green(pl.name) + ' has ' + chalk.green(pl.tracks.length) + ' tracks imported');
         });
-
-        var a = 0, b = false;
+        var indexToFetch = process.argv[2];
+        console.log('asking for ' + chalk.yellow(indexToFetch));
+        if(!indexToFetch){
+            return false;
+        }
         _.each(allDatas, function(pl, key){
-            if(a){
-                return false;
-            }
-            a++;
-            console.log('Started export for ' + chalk.red(pl.name) + ' ...');
-            var OUTPUT = [], INPUT=0;
-            _.each(pl.tracks, function(track){
-                if(track){
-                    INPUT++;
-                    request(
-                        { method: 'GET',
-                        uri: spotiUrlSearchFor(track.artist, track.title),
-                        json: true,
-                        gzip: true
-                    }
-                    , function (error, response, body) {
-                        if(body && body.tracks){
-                            var el = _.first(body.tracks.items);
-                            if(el){
-                                OUTPUT.push('spotify:track:' + el.id);
-                            }else{
-                                OUTPUT.push('ERROR');   
-                            }
-                        }else{
-                            OUTPUT.push('ERROR');
+            if(indexToFetch === pl.name){
+                console.log('Started export for ' + chalk.red(pl.name) + ' ...');
+                var OUTPUT = [], INPUT=0;
+                _.each(pl.tracks, function(track){
+                    if(track){
+                        INPUT++;
+                        request(
+                            { method: 'GET',
+                            uri: spotiUrlSearchFor(track.artist, track.title),
+                            json: true,
+                            gzip: true
                         }
-                        check(OUTPUT, INPUT, pl);
+                        , function (error, response, body) {
+                            if(body && body.tracks){
+                                var el = _.first(body.tracks.items);
+                                if(el){
+                                    OUTPUT.push('spotify:track:' + el.id);
+                                }else{
+                                    console.log(chalk.red('/!\\ ') + 'failed for ' + track.artist + ' - ' + track.title);
+                                    OUTPUT.push('ERROR');   
+                                }
+                            }else{
+                                OUTPUT.push('ERROR');
+                                console.log(chalk.red('/!\\/!\\/!\\ ') + 'failed for ' + track.artist + ' - ' + track.title);
+                            }
+                            check(OUTPUT, INPUT, pl);
+                        }
+                        );
                     }
-                    );
-                }
-            });
-        });
+                });
+}
+});
 });
 
+var progress = '';
 var check = function(OUTPUT, INPUT, pl){
-    console.log(OUTPUT.length, INPUT);
+    if(_.last(OUTPUT)==='ERROR'){
+        progress += chalk.red('X');
+    }else{
+        progress += chalk.green('=');
+    }
     if(OUTPUT.length === INPUT){
+        console.log('[' + progress +']');
+        console.log('');
+        console.log('');
+        console.log('');
         console.log(_.without(OUTPUT, 'ERROR').join(','));        
+        console.log('');
+        console.log('');
+        console.log('');
         console.log('Ended export for ' + chalk.green(pl.name) + ' !');
     }
 }
