9c41705f2c22684b62ca202503ce64d3e4252696
Clément LAMOUREUX <clement@raccourci.fr>
2015-04-17 17:30:46 +0200

    Working output !

--- src/run.js
+++ src/run.js
@@ -8,50 +8,79 @@
  var _ = require('lodash');
  var chalk = require('chalk');
  var PlayMusic = require('../');
+ var request = require('request');
 
- var pm = new PlayMusic();
- var config = JSON.parse(fs.readFileSync("config.json"));
- pm.init(config, function() {
-    // pm.getLibrary(function(library) {
-    //     var song = library.data.items.pop();
-    //     console.log(song);
-    //     pm.getStreamUrl(song.id, function(streamUrl) {
-    //         console.log(streamUrl);
-    //     });
-    // });
+ var spotiUrlSearchFor = function(artist, title){
+   return "https://api.spotify.com/v1/search?q=" + artist + "+-+" + title + "&type=track";
+}
 
-    // pm.search("bastille lost fire", 5, function(data) {
-    //     var song = data.entries.sort(function(a, b) {
-    //         return a.score < b.score;
-    //     }).shift();
-    //     console.log(song);
-    //     pm.getStreamUrl(song.track.nid, function(streamUrl) {
-    //         console.log(streamUrl);
-    //     });
-    // }, function(err) {
-    //     console.log(err);
-    // });
- var allDatas = {};
+var pm = new PlayMusic();
+var config = JSON.parse(fs.readFileSync("config.json"));
+pm.init(config, function() {
+    var allDatas = {};
 
- pm.getPlayLists(function(data) {
-    _.each(data.data.items, function(data){
-        var id = data.id;
-        allDatas[id]=data;
-        allDatas[id].tracks = [];
+    pm.getPlayLists(function(data) {
+        _.each(data.data.items, function(data){
+            var id = data.id;
+            allDatas[id]=data;
+            allDatas[id].tracks = [];
+        });
     });
+
+    pm.getPlayListEntries(function(data) {
+        _.each(data.data.items, function(item){
+            if(allDatas[item.playlistId]){
+                allDatas[item.playlistId].tracks.push(item.track);
+            }
+        });
+
+        _.each(allDatas, function(pl, key){
+            console.log('Playlist ' + chalk.green(pl.name) + ' has ' + chalk.green(pl.tracks.length) + ' tracks imported');
+        });
+
+        var a = 0, b = false;
+        _.each(allDatas, function(pl, key){
+            if(a){
+                return false;
+            }
+            a++;
+            console.log('Started export for ' + chalk.red(pl.name) + ' ...');
+            var OUTPUT = [], INPUT=0;
+            _.each(pl.tracks, function(track){
+                if(track){
+                    INPUT++;
+                    request(
+                        { method: 'GET',
+                        uri: spotiUrlSearchFor(track.artist, track.title),
+                        json: true,
+                        gzip: true
+                    }
+                    , function (error, response, body) {
+                        if(body && body.tracks){
+                            var el = _.first(body.tracks.items);
+                            if(el){
+                                OUTPUT.push('spotify:track:' + el.id);
+                            }else{
+                                OUTPUT.push('ERROR');   
+                            }
+                        }else{
+                            OUTPUT.push('ERROR');
+                        }
+                        check(OUTPUT, INPUT);
+                    }
+                    );
+                }
+            });
+        });
 });
 
- pm.getPlayListEntries(function(data) {
-    _.each(data.data.items, function(item){
-        if(allDatas[item.playlistId]){
-            allDatas[item.playlistId].tracks.push(item);
-        }
-    });
-
-    _.each(allDatas, function(pl, key){
-        console.log('Playlist ' + chalk.green(pl.name) + ' has ' + chalk.green(pl.tracks.length) + ' tracks imported');
-    });
-});
+var check = function(OUTPUT, INPUT){
+    console.log(OUTPUT.length, INPUT);
+    if(OUTPUT.length === INPUT){
+        console.log(_.without(OUTPUT, 'ERROR').join(','));        
+        console.log('Ended export for ' + chalk.green(pl.name) + ' !');
+    }
+}
 
     // pm.getStreamUrl("Thvfmp2be3c7kbp6ny4arxckz54", console.log);
 });
